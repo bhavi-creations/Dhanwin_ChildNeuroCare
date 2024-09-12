4a2e9a74cf34091be0a528357e97a2fdbc57071e
Ramesh92340 <rameshpilli1428@gmail.com>
2024-09-12 15:20:34 +0530

    databse updated

--- database/Dhanwin.sql
+++ database/Dhanwin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2024 at 12:36 PM
+-- Generation Time: Sep 12, 2024 at 11:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `oncologist`
+-- Database: `dhanwin`
 --
 
 -- --------------------------------------------------------
